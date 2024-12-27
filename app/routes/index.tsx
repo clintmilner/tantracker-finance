aa45ec5e77f197d283a4a77285385eb37a71fd46
Milner <cmilner@datto.com>
2024-12-27 15:15:31 +0000

    styling configs

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Button } from "@/components/ui/button"
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+  return (<div>Hello "/"!
+    <div>
+      <Button variant="outline">Test Button</Button>
+    </div>
+  </div>)
 }
