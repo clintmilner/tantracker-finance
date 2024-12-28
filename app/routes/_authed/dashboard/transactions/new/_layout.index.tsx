15bb182405f3c996bfd56d524a48e15ee2d46254
Clint Milner <clinton.milner@gmail.com>
2024-12-28 10:19:14 +0000

    adding card element for new transactions

--- app/routes/_authed/dashboard/transactions/new/_layout.index.tsx
+++ app/routes/_authed/dashboard/transactions/new/_layout.index.tsx
@@ -1,3 +1,5 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import TransactionForm from '@/components/ui/TransactionForm'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute(
@@ -7,5 +9,14 @@ export const Route = createFileRoute(
 })
 
 function RouteComponent() {
-  return <div>Hello "/_authed/dashboard/transactions/new/"!</div>
+  return (
+    <Card className={'max-w-screen-md mt-4'}>
+      <CardHeader>
+        <CardTitle>New Transaction</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <TransactionForm />
+      </CardContent>
+    </Card>
+  )
 }
