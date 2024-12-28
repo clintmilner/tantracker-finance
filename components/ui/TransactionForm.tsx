566e7c03b4256a86ded6c4b95f94241f1cafe6f4
clintmilner <clinton.milner@gmail.com>
2024-12-28 11:14:16 +0000

    transaction form is built and FE validation working

--- components/ui/TransactionForm.tsx
+++ components/ui/TransactionForm.tsx
@@ -1,5 +1,205 @@
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { cn } from '@/lib/utils'
+import { format } from 'date-fns'
+import { CalendarIcon, Inbox } from 'lucide-react'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const transactionFormSchema = z.object({
+  transactionType: z.enum(['income', 'expense']),
+  categoryId: z.coerce.number().positive('Please select a category'),
+  transactionDate: z
+    .date()
+    .max(new Date(), 'Transaction date cannot be in the future'),
+  amount: z.coerce.number().positive('Amount must greater than 0'),
+  description: z
+    .string()
+    .min(3, 'Description must be at least 3 characters')
+    .max(311, 'Description must contain a maximum of 311 characters'),
+})
+
+type FormData = z.infer<typeof transactionFormSchema>
+
 const TransactionForm = () => {
-  return <h1>TransactionForm</h1>
+  const form = useForm<FormData>({
+    resolver: zodResolver(transactionFormSchema),
+    defaultValues: {
+      transactionType: 'income',
+      amount: 0,
+      description: '',
+      categoryId: 0,
+      transactionDate: new Date(),
+    },
+  })
+
+  const handleSubmit = (data: FormData) => {
+    console.log(data)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)}>
+        <fieldset
+          disabled={form.formState.isSubmitting}
+          className={'grid grid-cols-2 gap-y-5 gap-x-2'}
+        >
+          <FormField
+            control={form.control}
+            name="transactionType"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Transaction Type</FormLabel>
+                  <FormControl>
+                    <Select value={field.value} onValueChange={field.onChange}>
+                      <SelectTrigger>
+                        <SelectValue placeholder={'Transaction type'} />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value={'income'}>Income</SelectItem>
+                        <SelectItem value={'expense'}>Expense</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />
+
+          <FormField
+            control={form.control}
+            name="categoryId"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Category</FormLabel>
+                  <FormControl>
+                    <Select
+                      value={field.value.toString()}
+                      onValueChange={field.onChange}
+                    >
+                      <SelectTrigger>
+                        <SelectValue placeholder={'Category'} />
+                      </SelectTrigger>
+                      <SelectContent>[empty]</SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />
+
+          <FormField
+            control={form.control}
+            name="transactionDate"
+            render={({ field: { onChange, value: date } }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Transaction Date</FormLabel>
+                  <FormControl>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant={'outline'}
+                          className={cn(
+                            'w-full justify-start text-left font-normal',
+                            !date && 'text-muted-foreground',
+                          )}
+                        >
+                          <CalendarIcon className="mr-2 h-4 w-4" />
+                          {date ? (
+                            format(date, 'PPP')
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0">
+                        <Calendar
+                          mode="single"
+                          selected={date}
+                          onSelect={onChange}
+                          initialFocus
+                          disabled={{
+                            after: new Date(),
+                          }}
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />
+
+          <FormField
+            control={form.control}
+            name="amount"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <Input {...field} type={'number'} step={0.01} min={0} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />
+        </fieldset>
+        <fieldset
+          disabled={form.formState.isSubmitting}
+          className={'mt-5 flex flex-col gap-5'}
+        >
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />
+          <Button type={'submit'}>
+            {form.formState.isSubmitting ? 'Submitting...' : 'Submit'}
+          </Button>
+        </fieldset>
+      </form>
+    </Form>
+  )
 }
 
 export default TransactionForm
